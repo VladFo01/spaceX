b4a975a35afb3e73119d9b15d929dc232be58000
Vlad <vladfo2003@gmail.com>
2021-01-22 10:17:52 +0200

    Updates

---
+++ precache-manifest.04db0a6bcfc49413cded7d8d6d3b378a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2f5b4c425650d6fce92e644150edf2",
+    "url": "/spaceX/index.html"
+  },
+  {
+    "revision": "b07bb93af9c1a9a15e19",
+    "url": "/spaceX/static/css/main.e42cb26b.chunk.css"
+  },
+  {
+    "revision": "6f469b00271c17f5e074",
+    "url": "/spaceX/static/js/2.dbba5314.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/spaceX/static/js/2.dbba5314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07bb93af9c1a9a15e19",
+    "url": "/spaceX/static/js/main.58482941.chunk.js"
+  },
+  {
+    "revision": "674d2da5a744fff73ed5",
+    "url": "/spaceX/static/js/runtime-main.bf6e654a.js"
+  }
+]);
